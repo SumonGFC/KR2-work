b1b195c93dd0506109b9b53c49cab173f62ad8e8
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-02-03 13:14:07 -0500

    Complete Exercise 1-21: Entab

--- ch1/Exercises/1-21_Entab.c
+++ ch1/Exercises/1-21_Entab.c
@@ -1,33 +1,44 @@
 /* Write a program 'entab' that replaces strings of blanks by the minimum 
  * number of tabs and blanks to achieve the same spacing. Use the same tab 
  * stops as for detab. When either a tab or a single blank would suffice to 
- * reach a tab stop, which should be given preference? */
+ * reach a tab stop, which should be given preference? 
+ * -- Blank gets preference */
 
 #include <stdio.h>
 
-#define TABSTOP 4
+#define TABSTOP 8
 #define SPACE ' '
+#define SPACE_PRINT '_'
 
 int main ()
 {
-    int c;
+    int c;  //store char in here
     int i;
-    int spaces = 0;
-
+    int tab_pos = 0;
+    int blank_len = 0;
+    
     while ((c = getchar()) != EOF) {
+        ++tab_pos;
         if (c != SPACE) {
-            for (i = 0; i < spaces; ++i)
-                putchar(SPACE);
-            putchar(c);
-            spaces = 0;
-        } else {
-            ++spaces;
-            if (spaces == TABSTOP) {
-                putchar('\t');
-                spaces = 0;
+            if (c == '\t')
+                printf("\\t");
+            else {
+                for (i = 0; i < blank_len; ++i)
+                    putchar(SPACE_PRINT);
+                blank_len = 0;
+                putchar(c);
+            }
+        }
+        else {
+            ++blank_len;
+            if (tab_pos % TABSTOP == 0) {
+                if (blank_len == 1)
+                    putchar(SPACE_PRINT);
+                else
+                    printf("\\t");
+                blank_len = 0;
             }
         }
     }
-
     return 0;
 }
