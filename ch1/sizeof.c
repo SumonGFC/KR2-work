09b226fb1b61f68d01eaf0fb18873b4cf70a72f3
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-03-16 15:32:29 -0400

    Update sizeof.c to print proper formatting

--- ch1/sizeof.c
+++ ch1/sizeof.c
@@ -4,24 +4,24 @@
 int main() {    
 
     // chars
-    printf("sizeof char: %lu\n bytes", sizeof(char));
-    printf("sizeof signed char: %lu\n bytes", sizeof(signed char));
-    printf("sizeof unsigned char: %lu\n bytes", sizeof(unsigned char));
+    printf("sizeof char: %lu bytes \n", sizeof(char));
+    printf("sizeof signed char: %lu bytes \n", sizeof(signed char));
+    printf("sizeof unsigned char: %lu bytes\n", sizeof(unsigned char));
     // shorts
-    printf("sizeof short: %lu\n bytes",sizeof(short));
-    printf("sizeof unsigned short: %lu\n bytes", sizeof(unsigned short));
+    printf("sizeof short: %lu bytes\n",sizeof(short));
+    printf("sizeof unsigned short: %lu bytes\n", sizeof(unsigned short));
     // ints
-    printf("sizeof int: %lu\n bytes",sizeof(int));
-    printf("sizeof unsigned int: %lu\n bytes",sizeof(unsigned int));
+    printf("sizeof int: %lu bytes\n",sizeof(int));
+    printf("sizeof unsigned int: %lu bytes\n",sizeof(unsigned int));
     //longs
-    printf("sizeof long: %lu\n bytes",sizeof(long));
-    printf("sizeof unsigned long: %lu\n bytes", sizeof(unsigned long));
-    printf("sizeof long long: %lu\n bytes", sizeof(long long));
-    printf("sizeof unsigned long long: %lu\n bytes", sizeof(unsigned long long));
+    printf("sizeof long: %lu bytes\n",sizeof(long));
+    printf("sizeof unsigned long: %lu bytes\n", sizeof(unsigned long));
+    printf("sizeof long long: %lu bytes\n", sizeof(long long));
+    printf("sizeof unsigned long long: %lu bytes\n", sizeof(unsigned long long));
     //floats
-    printf("sizeof float: %lu\n bytes", sizeof(float));
-    printf("sizeof doube: %lu\n bytes", sizeof(double));
-    printf("sizeof long double: %lu\n bytes", sizeof(long double));
+    printf("sizeof float: %lu bytes\n", sizeof(float));
+    printf("sizeof doube: %lu bytes\n", sizeof(double));
+    printf("sizeof long double: %lu bytes\n", sizeof(long double));
 
     return 0;
 }
