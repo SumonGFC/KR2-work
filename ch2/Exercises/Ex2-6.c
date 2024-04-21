f8da618eef81807fbe777aed7d67c217b92829dc
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-04-21 11:03:01 -0400

    Add comments and clean up

--- ch2/Exercises/Ex2-6.c
+++ ch2/Exercises/Ex2-6.c
@@ -4,21 +4,24 @@
  */
 #include <stdio.h>
 
-int setbits(unsigned x, unsigned int p, unsigned int n, unsigned y);
+int setbits(unsigned x, int p, int n, unsigned y);
 
 int main(void)
 {
     int i;
     for (i = 0; i < 8; ++i)
-        printf("0x%08x\n", setbits(0xffffffff, (i*4), 4, 0));
+        printf("0x%08x\n", setbits(0xffffffff, (i*4), 4, 0x69));
     return 0;
 }
 
-int setbits(unsigned x, unsigned int p, unsigned int n, unsigned y)
+int setbits(unsigned x, int p, int n, unsigned y)
 {
-    int mask_x = ~((~(~0U << n)) << p); // 0...01...10...0
+    // Use unsigned 0 (0U) so the compiler doesn't complain with -Wall -Wextra
+    // Otherwise, n will be promoted to a (signed) int -- possible negative left shift
+
+    int mask_x = ~((~(~0U << n)) << p); // 1...10...01...1
     int mask_y = ~(~0U << n);           // 0...01...1
-    x = x & mask_x;
-    y = (y & mask_y) << p;
+    x = x & mask_x;                     // x...x0...0x...x
+    y = (y & mask_y) << p;              // 0...0y...y0...0
     return x | y;
 }
